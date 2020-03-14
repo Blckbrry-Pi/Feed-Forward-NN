ba80f27e6bda1241b4a2c4b2bb887c00d0b195de
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-03-14 13:27:50 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <string>
 using std::cout;
 using std::endl;
 using std::string;
 using std::getline;
 
+class layer {
+	private:
+		long newrons;
+		double neurons[];
+	public:
+		newrons = 0;
+		neurons = {420.69, 340.1};
+};
+
 int main() {
     string j;
     string k;
     string l;
     string m;
-    
+    string n;
     std::ifstream params("../Settings:Storage/Parameters.txt");
     std::ofstream rawData("../Settings:Storage/Training Dump.neuraw");
     
@@ -29,14 +39,17 @@ int main() {
 			case 10:
                 		getline(params, m);
 				break;
-            case 10:
-                        getline(params, m);
-                break;
+           		case 14:
+                        	getline(params, n);
+                		break;
 		}
         
         
 	}
     
-	cout << "Hello, world!" << endl;
-	return 0;
+    std::vector<layer> yay;
+    for (int i = 0; i < yay.size(); i++) {
+	cout << yay.at(i) << endl;
+    }
+    return 0;
 }
